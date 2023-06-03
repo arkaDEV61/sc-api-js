b376ff424d95b035de57a706ec7cbee211e7fb03
Armin Causevic <armin61@gmail.com>
2023-06-04 00:16:37 +0200

    user activation request
    Took 3 minutes

---
+++ src/api/activation-request/controllers/activation-request.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * activation-request controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::activation-request.activation-request');
