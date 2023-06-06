089487f05251d4ee140ae4572e5e8634f7275679
Armin Causevic <armin61@gmail.com>
2023-06-06 08:44:45 +0200

    students user group added free days added
    Took 11 seconds

---
+++ src/api/free-day/controllers/free-day.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * free-day controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::free-day.free-day');
