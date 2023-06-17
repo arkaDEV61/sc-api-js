4997fed29f140d9d617aec318d645f192cdfd804
Armin Causevic <armin61@gmail.com>
2023-06-17 10:18:23 +0200

    db connection fix
    Took 30 seconds

--- config/database.js
+++ config/database.js
@@ -7,9 +7,9 @@ module.exports = ({ env }) => {
       connection: {
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'bank'),
+        database: env('DATABASE_NAME', 'db'),
         user: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', '0000'),
+        password: env('DATABASE_PASSWORD', '1234'),
         ssl: {
           rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
         },
